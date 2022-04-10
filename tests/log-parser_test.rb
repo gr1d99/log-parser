2da3d10e6e604ea78d97ce35dcfbd9c5b9502f75
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2022-04-10 06:00:01 +0300

    chore: fix failling tests

--- tests/log-parser_test.rb
+++ tests/log-parser_test.rb
@@ -18,20 +18,20 @@ class LogParserTest < MiniTest::Test
 
     total_views = result[:total_views]
 
-    assert_includes(total_views, { '/help_page/1': :"3 Visits" })
-    assert_includes(total_views, { '/contact': '1 Visit' })
-    assert_includes(total_views, { home: :"2 Visits" })
+    assert_includes(total_views, { "/help_page/1": '3 Visits' })
+    assert_includes(total_views, { "/contact": '1 Visit' })
+    assert_includes(total_views, { "/home": '2 Visits' })
   end
 
   def test_parses_correct_unique_views
     log_file = './tests/webserver.log'
     result = LogParser.call(log_file)
 
-    unique_views = result[:total_views]
+    unique_views = result[:unique_views]
 
-    assert_includes(unique_views, { '/help_page/1': :'2 Unique visits' })
-    assert_includes(unique_views, { '/contact': :'1 Unique visits' })
-    assert_includes(unique_views, { home: '2 Unique visits' })
+    assert_includes(unique_views, { "/help_page/1": '2 Unique Visits' })
+    assert_includes(unique_views, { "/contact": '1 Unique Visit' })
+    assert_includes(unique_views, { "/home": '2 Unique Visits' })
   end
 
   def test_assert_sorts_total_views_starting_with_most_views
@@ -39,9 +39,9 @@ class LogParserTest < MiniTest::Test
     result = LogParser.call(log_file)
     total_views = result[:total_views]
 
-    assert_equal(total_views[0], { '/help_page/1': :"3 Visits" })
-    assert_equal(total_views[1], { home: :"2 Visits" })
-    assert_equal(total_views[2], { '/contact': '1 Visit' })
+    assert_equal(total_views[0], { "/help_page/1": '3 Visits' })
+    assert_equal(total_views[1], { "/home": '2 Visits' })
+    assert_equal(total_views[2], { "/contact": '1 Visit' })
   end
 
   def test_assert_sorts_unique_views_starting_with_most_views
@@ -50,8 +50,8 @@ class LogParserTest < MiniTest::Test
 
     unique_views = result[:unique_views]
 
-    assert_equal(unique_views[0], { '/help_page/1': :'2 Unique visits' })
-    assert_equal(unique_views[2], { home: '2 Unique visits' })
-    assert_equal(unique_views[1], { '/contact': :'1 Unique visits' })
+    assert_equal(unique_views[0], { "/help_page/1": '2 Unique Visits' })
+    assert_equal(unique_views[1], { "/home": '2 Unique Visits' })
+    assert_equal(unique_views[2], { "/contact": '1 Unique Visit' })
   end
 end
